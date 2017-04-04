43e885f4ee9672f3034ca44c1ff5fab93c923046
rhinophile <bijan.text@gmail.com>
2017-04-04 15:47:14 -0400

    works

--- heatCan.R
+++ heatCan.R
@@ -1,23 +1,38 @@
 library('rjags')
 library(data.table)
-source('loaddata.R')
 
+load('~/Projects/transFlux/bothSites.Rdata')
+df <- na.omit(transData[Site=='PK', .(TA, RgOpen, Rg, LST, WS, ET, TR)])
+dim(df)
+# df[,plot(Rg, Sdifopen)]
+# abline(0,1)
 jags <- jags.model('heatCan.bugs',
-                   data = list('Si' = data$Si,
-                               'Tair' = data$Tair,
-                               'Tsir' = data$Tsur,
-                               'WS' = data$WS,
-                               'ET' = data$ET,
-                               'n' = n),
+                   data = list('Si' = df$Rg,
+                               'Tair' = df$TA,
+                               'Tsur' = df$LST,
+                               'WS' = df$WS,
+                               'ET' = df$ET,
+                               'n' = nrow(df)),
                    n.chains = 1,
-                   n.adapt = 50)
+                   n.adapt = 50, quiet = T)
 
 update(jags, 1000)
 
 samples <- jags.samples(jags,n.iter = 1000,
-                        c('albedo','albedo','eSur',
-                          'Cgrnd','Cconv','Aconv',
-                          'tau','sigma'))
+                        c('albedo','eSur','eSky',
+                          'Cgrnd',
+                          'Cconv','Aconv',
+                          'sigma'))
 
-boxplot(samples, outline = F)
+
+
+preds <- jags.samples(jags,n.iter = 1000, 'ETpred')
+
+pred <- apply(preds$ETpred, 1, mean)
+plot(df$ET, pred)
+abline(0,1,col='red')
+lm(df$ET~ pred-1)
+
+print(signif(sapply(samples, mean)),2)
+boxplot(samples[-2], outline = F)
 
