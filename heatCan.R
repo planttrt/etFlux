ee1fcde9ecca3d7cc267a3ed2aaeca724fe62c30
rhinophile <bijan.text@gmail.com>
2017-04-05 20:30:08 -0400

    works better

--- heatCan.R
+++ heatCan.R
@@ -1,8 +1,12 @@
 library('rjags')
 library(data.table)
+source('~/Projects/procVisData/dataViz.R')
 
 load('~/Projects/transFlux/bothSites.Rdata')
-df <- na.omit(transData[Site=='PK', .(TA, RgOpen, Rg, LST, WS, ET, TR)])
+df <- na.omit(transData[Site=='PK', .(TA, RgOpen, Rg, LST, WS, Tr= ET)])
+df <- na.omit(transData[Site=='PK', .(TA, RgOpen, Rg, LST, WS, Tr= TR)])
+# df <- na.omit(transData[Site=='PK',])
+# df <- na.omit(transData[Site=='BW', .(TA, RgOpen, Rg, LST, WS, Tr= TR)])
 dim(df)
 # df[,plot(Rg, Sdifopen)]
 # abline(0,1)
@@ -11,28 +15,37 @@ jags <- jags.model('heatCan.bugs',
                                'Tair' = df$TA,
                                'Tsur' = df$LST,
                                'WS' = df$WS,
-                               'ET' = df$ET,
+                               'ET' = df$Tr,
                                'n' = nrow(df)),
-                   n.chains = 1,
-                   n.adapt = 50, quiet = T)
+                   # n.chains = 1,
+                   # n.adapt = 50, 
+                   quiet = T)
 
-update(jags, 1000)
+update(jags, 1)
 
-samples <- jags.samples(jags,n.iter = 1000,
-                        c('albedo','eSur','eSky',
-                          'Cgrnd',
-                          'Cconv','Aconv',
-                          'sigma'))
-
-
-
-preds <- jags.samples(jags,n.iter = 1000, 'ETpred')
-
-pred <- apply(preds$ETpred, 1, mean)
-plot(df$ET, pred)
-abline(0,1,col='red')
-lm(df$ET~ pred-1)
-
+samples <- jags.samples(jags,n.iter = 5000,
+                        c('albedo',
+                          'eta',
+                          'eSur',
+                          'eSky',
+                           # 'Cgrnd',
+                          'Cconv',
+                           'Aconv',
+                          'sigma',
+                          'ETpred',  
+                          'ETobs'))
+ 
+plot(samples$albedo)
+#plot(samples$Cconv)
 print(signif(sapply(samples, mean)),2)
-boxplot(samples[-2], outline = F)
+
+pred <- apply(samples$ETpred, 1, mean)
+obs <- apply(samples$ETobs, 1, mean)
+
+plotObsPred(obs, pred, nbin = 10, breaks = seq(0,9,1), xlim = c(0.5,9), ylim=c(0.5,9))
+abline(0,1,col='red')
+plotObsPred(obs, pred, nbin = 10, breaks = seq(0,14,1.5), xlim = c(2,14), ylim=c(2,14))
+abline(0,1,col='red')
+print(cor(obs, pred)^2)
+#boxplot(samples[-2], outline = F)
 
