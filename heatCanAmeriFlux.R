98fb5e4599be18b0a9d4b0de0159fdaf3ddc5f5a
rhinophile <bijan.text@gmail.com>
2017-04-13 13:37:06 -0400

    works great for all sites

--- heatCanAmeriFlux.R
+++ heatCanAmeriFlux.R
@@ -1,32 +1,22 @@
-LST[,UniCode:=paste(Site, Year,
-                    sprintf(DOY,fmt = '%03d'), 
-                    sprintf(floor(Hour*2),fmt = '%02d'), sep = '.' )]
-ameri[,UniCode:=paste(Site, Year,
-                      sprintf(DOY,fmt = '%03d'), 
-                      sprintf(floor(Hour*2)+1,fmt = '%02d'), sep = '.' )]
-
-LST
-ameri
-
-ameriLST <- merge(LST[,.(Site, Year, DOY, LST=LST_Day_1km, Emissivity, UniCode)], 
-                  ameri[,.(TA, RS, WS, ET, UniCode)], by = 'UniCode')
-# ameriLST[,plot(Year+DOY/365, ET, col=Site)]
-ameriLST <- na.omit(ameriLST)
-
 
 table(ameriLST$Site)
 # ChR Dk2 Dk3 NC2
 
-df <- ameriLST[Site=='KS2', ]
+#df <- ameriLST[Site=='KS2', ]
+df <- ameriLST[Site%in%c('ChR', 'Dk2', 'Dk3', 'NC2')]
+# df <- ameriLST[Site%in%c('ChR', 'Dk2')]
 dim(df)
 
 
-jags <- jags.model('heatCan.bugs',
+
+jags <- jags.model('heatCanSites.bugs',
                    data = list('Si' = df$RS,
                                'Tair' = df$TA,
                                'Tsur' = df$LST,
                                'WS' = df$WS,
                                'ET' = df$ET,
+                               'Sites' = as.numeric(as.factor(as.character(df$Site))),
+                               'ns' = length(unique(df$Site)),
                                'n' = nrow(df)),
                    n.chains = 1,
                    quiet = T)
@@ -51,14 +41,15 @@ samples[c('abs',
           'Aconv',
           'sigma')]
 
-#albedo = .2, eSky = .70, eSur = .95, Cconv = 10, Aconv = .8, eta = .50, sigma = 1
+
+samples[c("Aconv","Cconv", "abs", "eSky","eSur","sigma")]
 
 pred <- apply(samples$ETpred, 1, mean)
 obs <- apply(samples$ETobs, 1, mean)
 
 #ETpred <- predictET(df, samples)
 
-plotObsPred(obs, pred, nbin = 15)
+plotObsPred(obs, pred, nbin = 15, ptcol = as.numeric(as.factor(as.character(df$Site))))
 abline(0,1,col='red')
 lm(pred~obs-1)
 print(cor(obs, pred))
